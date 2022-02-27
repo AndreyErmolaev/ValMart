c29383110a7c275997c9ff32c6ab32fe01a0d2a7
AndreyErmolaev <astralinkin@gmail.com>
2022-02-27 22:30:53 +0700

    main page + footer end

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$('.courses__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    nextArrow:'<button type="button" class="slick-next card__arrow"><img src="images/icons/arrow-left.svg" alt="left"></button>',
+    prevArrow:'<button type="button" class="slick-prev card__arrow"><img src="images/icons/arrow-right.svg" alt="left"></button>',
+});
+
+$('.master__slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    nextArrow:'<button type="button" class="slick-next card__arrow"><img src="images/icons/arrow-left.svg" alt="left"></button>',
+    prevArrow:'<button type="button" class="slick-prev card__arrow"><img src="images/icons/arrow-right.svg" alt="left"></button>',
+});
+
+$('.offer__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    nextArrow:'<button type="button" class="slick-next card__arrow"><img src="images/icons/arrow-left.svg" alt="left"></button>',
+    prevArrow:'<button type="button" class="slick-prev card__arrow"><img src="images/icons/arrow-right.svg" alt="left"></button>',
+});
